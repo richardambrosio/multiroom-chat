d12d351cb75baf3d75be39a35127d2f4fc9b2d73
Richard Ambrosio <richard.ambrosio1@live.com>
2021-05-10 14:15:16 -0300

    adicionando socket.io ao projeto

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 var app = require('./config/server');
 
 //Parametrizar a porta de escuta
-app.listen(8080, function(){
+var server = app.listen(8080, function(){
     console.log('server on');
 });
+
+//adicionando o socket.io para ouvir as requisições, na mesma porta http
+require('socket.io').listen(server);
