7c64eb7c198f32a9bfdd0b0d6a1a8855ae26ec67
Richard Ambrosio <richard.ambrosio1@live.com>
2021-05-10 13:55:21 -0300

    recebendo e validando os dados do formulário de entrada

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,18 @@
 module.exports.iniciaChat = function(application, request, response){
+    
+    //Recuperando os dados com bodyParser
+    var dadosForm = request.body;
+    
+    //Usando e express validator para validar o campo apelido
+    request.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
+    request.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+    
+    var erros = request.validationErrors();
+
+    if(erros) {
+        response.render("index", {validacao: erros});
+        return;
+    }
+    
     response.render('chat');
 }
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,4 @@
 module.exports.home = function(application, request, response){
     
-    response.render("index");
+    response.render("index", {validacao: {}});
 }
