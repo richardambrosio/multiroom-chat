156050d9268beae946e71f90c15f42a5fe993f01
Richard Ambrosio <richard.ambrosio1@live.com>
2021-05-10 11:49:36 -0300

    configurando variáveis do server

--- config/server.js
+++ config/server.js
@@ -13,5 +13,25 @@ var expressValidator = require('express-validator');
 //Iniciar objeto do express
 var app = express();
 
+//Setar as variáveis 'view engine' e 'views' do express
+app.set('view engine', 'ejs');
+app.set('views', './app/views'); //indica onde está as views do projeto
+
+//Configurar o middleware express.static
+app.use(express.static('./app/public')); //indica onde estarão os arquivos estáticos(css, images, etc)
+
+//Configurar o middleware body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Configurar o midlewarre express-validator
+app.use(expressValidator());
+
+//Fazendo o autoload das rotas, models e controllers para o objeto app
+consign()
+    .include('app/routes')
+    .then('app/models')
+    .then('app.controllers')
+    .into(app);
+
 //Exportar o objeto app
 module.exports = app;
